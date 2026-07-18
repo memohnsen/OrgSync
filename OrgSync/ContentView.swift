afd4354bd14073b949d307aadd13a1a42927b0a6
Maddisen <memohnsen@gmail.com>
2026-07-18 09:20:39 -0400

    Initial Commit

---
+++ OrgSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OrgSync
+//
+//  Created by Maddisen Mohnsen on 7/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OrgSync/OrgSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrgSyncApp.swift
+//  OrgSync
+//
+//  Created by Maddisen Mohnsen on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct OrgSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
